924ff296e0d4b5251294a7ff4dff82114741eb4e
radupasparuga <radupasparuga25@gmail.com>
2021-02-12 20:37:37 +0200

    Add basic interface

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,62 @@
 extern crate iui;
 use iui::prelude::*;
-use iui::controls::{Entry, Label, VerticalBox};
+use iui::controls::{Entry, VerticalBox, Group, Label, HorizontalBox};
+use std::rc::Rc;
+use std::cell::RefCell;
 
+struct State {
+    entry_val: String,
+}
 
 fn main() {
     let ui = UI::init().expect("Couldn't initialize UI library");
-    let mut win = Window::new(&ui, "Asciimoji", 600, 200, WindowType::NoMenubar);
+    let state = Rc::new(RefCell::new(State { entry_val: "".into() }));
 
-    let mut vbox = VerticalBox::new(&ui);
-    vbox.set_padded(&ui, true);
+    let (input_group, mut entry) = {
+        let mut input_group = Group::new(&ui, "Inputs");
+        let mut input_vbox = VerticalBox::new(&ui);
+        input_vbox.set_padded(&ui, true);
 
-    let mut label_text = String::new();
-    label_text.push_str("Search for an asciimoji below!\n");
-    let label = Label::new(&ui, &label_text);
+        let entry = Entry::new(&ui);
+        input_vbox.append(&ui, entry.clone(), LayoutStrategy::Compact);
 
-    let mut search = Entry::new(&ui);
+        input_group.set_child(&ui, input_vbox);
+        (input_group, entry)
+    };
 
-    vbox.append(&ui, label, LayoutStrategy::Stretchy);
-    vbox.append(&ui, search, LayoutStrategy::Stretchy);
+    let (output_group, text_label) = {
+        let mut output_group = Group::new(&ui, "Outputs");
+        let mut output_vbox = VerticalBox::new(&ui);
+        let text_label = Label::new(&ui, "");
 
-    win.set_child(&ui, vbox);
-    win.show(&ui);
-    ui.main();
+        output_vbox.append(&ui, text_label.clone(), LayoutStrategy::Compact);
+        output_group.set_child(&ui, output_vbox);
+        (output_group, text_label)
+    };
+
+    let mut hbox = HorizontalBox::new(&ui);
+    hbox.append(&ui, input_group, LayoutStrategy::Stretchy);
+    hbox.append(&ui, output_group, LayoutStrategy::Stretchy);
+
+    let mut window = Window::new(&ui, "Input output Test", 1280, 720, WindowType::NoMenubar);
+    window.set_child(&ui, hbox);
+    window.show(&ui);
+
+    entry.on_changed(&ui, {
+        let state = state.clone();
+        move |val| { state.borrow_mut().entry_val = val; }
+    });
+
+    let mut event_loop = ui.event_loop();
+    event_loop.on_tick(&ui, {
+        let ui = ui.clone();
+        let mut text_label = text_label.clone();
+        move || {
+            let state = state.borrow();
+
+            text_label.set_text(&ui, &format!("Text: {}", state.entry_val));
+        }
+    });
+
+    event_loop.run(&ui);
 }
